143d8b0056318325ac68cbbaf73238f4d5ac75d1
Nop Jiarathanakul <nop@iamnop.com>
2015-06-18 22:17:46 -0700

    fix jshint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var getParameterByName = function(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-  var results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-};
+// var getParameterByName = function(name) {
+//   name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+//   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+//   var results = regex.exec(location.search);
+//   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+// };
 
 (function() {
   var app = document.querySelector("#app");
